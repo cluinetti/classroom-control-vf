8d7383fc2be112e2785514f401c4943f4f6f6ee9
cluinetti <cluinetti@deloitte.com>
2016-03-29 09:18:50 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  include users
   notify { "Hello, my name is ${::hostname}": }
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
     path => '/usr/local/bin',
