33b530968889959f7104e7304a6273fbd2bde51f
cluinetti <cluinetti@deloitte.com>
2016-03-30 14:55:26 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,7 +44,7 @@ node default {
   #   class { 'my_class': }
   include users
   #include skeleton
-  include admins
+  include users::admins
   notify { "Hello, my name is ${::hostname}": }
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
     path => '/usr/local/bin',
