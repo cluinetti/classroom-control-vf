c9dfdadf051bb7b4854cf61729c8f86efff4f4d4
cluinetti <cluinetti@deloitte.com>
2016-04-01 16:59:07 -0500

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,26 +1,38 @@
 define users::managed_user (
   $shell = '/bin/bash',
   $home = "/home/${title}",
-  $group = $title,
   $ssh_authkey = undef,
   $ssh_authkey_type = 'ssh-rsa',
 ) {
   $user = $title
   File {
     owner => $user,
-}
+  }
   user { $user:
-   ensure => present,
-   shell => $shell,
-}
- file { "$home/${title}":
-  ensure => directory,
-  owner => $title,
-  group => $group,
-  mode => '0750',
-}
-file { "${home}/.ssh":
-  ensure => directory,
-  mode => '0700',
+    ensure => present,
+    home => $home,
+    shell => $shell,
+  }
+  file { $home:
+    ensure => directory,
+    mode => '0750',
+  }
+  file { "${home}/.ssh":
+    ensure => directory,
+    mode => '0700',
+  }
+  file { "${home}/.vimrc":
+    ensure => file,
+    replace => false,
+    source => 'puppet:///modules/users/vimrc',
+  }
+  if $ssh_authkey {
+    ssh_authorized_key { "${user} default key":
+      ensure => present,
+      user => $user,
+      target => "${home}/.ssh/authorized_keys2",
+      type => $ssh_authkey_type,
+      key => $ssh_authkey,
+    }
   }
 }
